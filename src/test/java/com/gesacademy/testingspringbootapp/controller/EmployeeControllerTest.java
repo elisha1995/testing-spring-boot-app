ad7a905af91f72ed657c5fafb86f02088ef1c5a2
elisha1995 <elishasamuel01@gmail.com>
2025-03-09 22:52:37 +0000

    test(code): Unit test for updateEmployee API in EmployeeController

--- src/test/java/com/gesacademy/testingspringbootapp/controller/EmployeeControllerTest.java
+++ src/test/java/com/gesacademy/testingspringbootapp/controller/EmployeeControllerTest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gesacademy.testingspringbootapp.model.Employee;
 import com.gesacademy.testingspringbootapp.service.EmployeeService;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -14,9 +13,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import java.util.List;
 import java.util.Optional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
@@ -24,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 
 import static org.mockito.BDDMockito.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
 
 
 
@@ -50,7 +48,7 @@ class EmployeeControllerTest {
                 .email("john.doe@example.com")
                 .build();
 
-        given(employeeService.saveEmployee(ArgumentMatchers.any(Employee.class)))
+        given(employeeService.saveEmployee(any(Employee.class)))
                 .willAnswer(invocation -> invocation.getArgument(0));
 
         // when - action or behaviour that we are going test
@@ -126,4 +124,76 @@ class EmployeeControllerTest {
         response.andExpect(status().isNotFound())
                 .andDo(print());
     }
+
+    // test for update employee REST API (positive scenario)
+    @Test
+    void givenUpdatedEmployee_whenUpdateEmployee_thenReturnUpdateEmployeeObject() throws Exception {
+
+        // given - precondition or setup
+        Employee savedEmployee = Employee.builder()
+                .id(1L)
+                .firstName("John")
+                .lastName("Doe")
+                .email("john.doe@example.com")
+                .build();
+
+        Employee updatedEmployee = Employee.builder()
+                .id(1L)
+                .firstName("Jane")
+                .lastName("Smith")
+                .email("jane.smith@example.com")
+                .build();
+
+        given(employeeService.getEmployeeById(savedEmployee.getId())).willReturn(Optional.of(savedEmployee));
+        given(employeeService.updateEmployee(any(Employee.class)))
+                .willAnswer(invocation -> invocation.getArgument(0));
+
+        // when - action or behaviour that we are going test
+        ResultActions response = mockMvc.perform(put("/api/v1/employees/{id}", savedEmployee.getId())
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(updatedEmployee)));
+
+        // then - verify the output
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.firstName", is(updatedEmployee.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(updatedEmployee.getLastName())))
+                .andExpect(jsonPath("$.email", is(updatedEmployee.getEmail())));
+    }
+
+    // test for update employee REST API (negative scenario)
+    @Test
+    void givenUpdatedEmployee_whenUpdateEmployee_thenReturn404() throws Exception {
+
+        // given - precondition or setup
+        Employee savedEmployee = Employee.builder()
+                .id(1L)
+                .firstName("John")
+                .lastName("Doe")
+                .email("john.doe@example.com")
+                .build();
+
+        Employee updatedEmployee = Employee.builder()
+                .id(1L)
+                .firstName("Jane")
+                .lastName("Smith")
+                .email("jane.smith@example.com")
+                .build();
+
+        given(employeeService.getEmployeeById(savedEmployee.getId())).willReturn(Optional.empty());
+        given(employeeService.updateEmployee(any(Employee.class)))
+                .willAnswer(invocation -> invocation.getArgument(0));
+
+        // when - action or behaviour that we are going test
+        ResultActions response = mockMvc.perform(put("/api/v1/employees/{id}", savedEmployee.getId())
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(updatedEmployee)));
+
+        // then - verify the output
+        response.andExpect(status().isNotFound())
+                .andDo(print());
+    }
+
+    // test for delete employee REST API
+
 }
