26a6fdb75442b236b5699c612341327571960bd4
elisha1995 <elishasamuel01@gmail.com>
2025-03-09 16:23:03 +0000

    Create EmployeeService with saveEmployee method

---
+++ src/main/java/com/gesacademy/testingspringbootapp/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.gesacademy.testingspringbootapp.service;
+
+import com.gesacademy.testingspringbootapp.model.Employee;
+
+public interface EmployeeService {
+    Employee saveEmployee(Employee employee);
+}
---
+++ src/main/java/com/gesacademy/testingspringbootapp/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,30 @@
+package com.gesacademy.testingspringbootapp.service.impl;
+
+import com.gesacademy.testingspringbootapp.exception.ResourceNotFoundException;
+import com.gesacademy.testingspringbootapp.model.Employee;
+import com.gesacademy.testingspringbootapp.repository.EmployeeRepository;
+import com.gesacademy.testingspringbootapp.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public Employee saveEmployee(Employee employee) {
+
+        Optional<Employee> savedEmployee = employeeRepository.findByEmail(employee.getEmail());
+        if (savedEmployee.isPresent()) {
+            // this shouldn't be ResourceNotFoundException
+            throw new ResourceNotFoundException("Employee already exist with given email:" + employee.getEmail());
+        }
+        return employeeRepository.save(employee);
+    }
+}
